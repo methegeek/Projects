ef1117d6252c4470f2f41c789bc5c79e6ad37b4a
sid <siddhanth.bangera68@gmail.com>
2020-03-16 22:12:51 +0000

    Debugmode 14

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -71,7 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    x
+    
 ]
 
 ROOT_URLCONF = 'reddit_clone.urls'
