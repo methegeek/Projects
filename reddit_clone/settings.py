382f87a3b6820d6e23c4025a7f1766e2782677ec
sid <siddhanth.bangera68@gmail.com>
2020-03-16 01:57:02 +0000

    Set Debug Value

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%$97#$a!3#vro0=h0tupyte^=$f^@y#*#+4n@tmo%ku&!nol5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['sidsblog.herokuapp.com']
 
