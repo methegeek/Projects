80bb1227ffa18843d6709617128831c19dbc6230
sid <siddhanth.bangera68@gmail.com>
2020-03-17 14:40:03 +0000

    Debugmode 31

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -41,8 +41,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%$97#$a!3#vro0=h0tupyte^=$f^@y#*#+4n@tmo%ku&!nol5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True 
-DEBUG404 = True 
+DEBUG =False 
+#DEBUG404 = True 
 #ALLOWED_HOSTS = ['*'] # it works but not secure, so use
 
 ALLOWED_HOSTS = ['sidsblog.herokuapp.com'] #if you are running locally, then run with python manage.py runserver --insecure.You can give your webserver here.
