43f1ae6a0bea20a137fbfbd23e549528dd83d1aa
sid <siddhanth.bangera68@gmail.com>
2020-04-15 17:17:33 +0100

    security 7

--- users/models.py
+++ users/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self,**kwargs):
            im = Image.open(self.image)
            output = BytesIO()
            im = im.resize((100, 100))
