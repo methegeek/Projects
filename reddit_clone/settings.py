46e0c6678c1c4d894458a66ec2e538c81d8c379d
sid <siddhanth.bangera68@gmail.com>
2020-04-16 20:43:45 +0100

    SSL

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -191,5 +191,6 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 CSRF_COOKIE_SECURE = True #to avoid transmitting the CSRF cookie over HTTP accidentally.
 SESSION_COOKIE_SECURE = True #to avoid transmitting the session cookie over HTTP accidentally.
-#SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 django_heroku.settings(locals())
