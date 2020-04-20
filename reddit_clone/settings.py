7050497ba029639480a2e0318a7e42b00b7a3e25
sid <siddhanth.bangera68@gmail.com>
2020-04-20 01:58:21 +0100

    argon

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -144,7 +144,11 @@ AUTHENTICATION_BACKENDS = [
     # Django ModelBackend is the default authentication backend.
     'django.contrib.auth.backends.ModelBackend',
 ]
-
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 
 # Internationalization
