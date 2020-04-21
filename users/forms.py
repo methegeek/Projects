241a088ac8748985fb243d7cfb35c0f2cbfe546d
sid <siddhanth.bangera68@gmail.com>
2020-04-21 15:09:38 +0100

    email1

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from.models import Profile
+from django.core.exceptions import ValidationError
 
 
 
@@ -13,6 +14,12 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def clean_email(self):
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exclude(username=username).count():          
+            raise forms.ValidationError('This email is already in use! Try another email.')
+        return email
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
--- users/views.py
+++ users/views.py
@@ -8,13 +8,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            try:
-                emailvalue= form.cleaned_data.get("email")
-                user= UserRegisterForm.objects.get(email=emailvalue)
-                context= {'form': form, 'error':'The email you entered has already been taken. Please try another email.'}
-                return render(request, 'users/register.html', {'form': form})
-            except:
-                form.save()
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
