03d9f7c94d81771019cbae3cb28f6215b27f7446
sid <siddhanth.bangera68@gmail.com>
2020-04-21 14:29:42 +0100

    email1

--- users/forms.py
+++ users/forms.py
@@ -13,7 +13,7 @@ class UserForm(forms.ModelForm):
 
     def clean_email(self):
         # Get the email
-        email = self.cleaned_data.get('email')
+        email = forms.EmailField()
 
         # Check to see if any users already exist with this email as a username.
         try:
